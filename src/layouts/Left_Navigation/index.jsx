59142f4fc92c62e35d4506da81b9ea1fb796fa4f
S4LUD <salud.lanceivan@gmail.com>
2022-10-20 20:09:53 +0800

    Create Vite@Latest React

--- src/layouts/Left_Navigation/index.jsx
+++ src/layouts/Left_Navigation/index.jsx
@@ -5,6 +5,12 @@ import {
   AiOutlinePlusSquare,
   BsSun,
   BsMoonStars,
+  AiOutlineProject,
+  BsListTask,
+  MdTimer,
+  HiOutlineUsers,
+  AiOutlineSchedule,
+  IoIosAnalytics,
 } from "react-icons/all";
 
 export default function Left_Navigtaion() {
@@ -13,17 +19,32 @@ export default function Left_Navigtaion() {
   const mediaQuery = gsap.matchMedia();
 
   useLayoutEffect(() => {
+    const SIDEBAR = localStorage.getItem("SIDEBAR");
+
+    if (SIDEBAR === "close") setToggle(false);
+
     (async () => {
       await mediaQuery.add(
         {
           isDesktop: "(max-width: 990px)",
         },
         () => {
+          localStorage.setItem("SIDEBAR", "close");
           setToggle(false);
         }
       );
     })();
   }, [mediaQuery]);
+
+  const HandleToggle = () => {
+    if (isToggle) {
+      localStorage.setItem("SIDEBAR", "close");
+      setToggle(!isToggle);
+    } else {
+      localStorage.removeItem("SIDEBAR");
+      setToggle(!isToggle);
+    }
+  };
 
   return (
     <div className={LNCSS._wrap_left_navigations}>
@@ -35,7 +56,7 @@ export default function Left_Navigtaion() {
                 <div className={LNCSS._sub_title}>workspace</div>
                 <div
                   className={LNCSS._toggle_icon}
-                  onClick={() => setToggle(!isToggle)}
+                  onClick={() => HandleToggle()}
                 >
                   <AiOutlineMinusSquare size={20} color="#363636" />
                 </div>
@@ -45,12 +66,30 @@ export default function Left_Navigtaion() {
               </div>
             </div>
             <div className={LNCSS._wrap_btn}>
-              <div className={LNCSS._btn}>Project</div>
-              <div className={LNCSS._btn}>Task</div>
-              <div className={LNCSS._btn}>Time Tracking</div>
-              <div className={LNCSS._btn}>Member</div>
-              <div className={LNCSS._btn}>Schedule</div>
-              <div className={LNCSS._btn}>Analytics</div>
+              <div className={LNCSS._btn}>
+                <AiOutlineProject size={20} />
+                Project
+              </div>
+              <div className={LNCSS._btn}>
+                <BsListTask size={20} />
+                Task
+              </div>
+              <div className={LNCSS._btn}>
+                <MdTimer size={20} />
+                Time Tracking
+              </div>
+              <div className={LNCSS._btn}>
+                <HiOutlineUsers size={20} />
+                Member
+              </div>
+              <div className={LNCSS._btn}>
+                <AiOutlineSchedule size={20} />
+                Schedule
+              </div>
+              <div className={LNCSS._btn}>
+                <IoIosAnalytics size={20} />
+                Analytics
+              </div>
             </div>
           </div>
           <div className={LNCSS._mode_style}>
@@ -68,9 +107,29 @@ export default function Left_Navigtaion() {
         <div className={LNCSS._small_ln_container}>
           <div
             className={`${LNCSS._toggle_icon_minimize}  ${LNCSS._hide_toggle_icon}`}
-            onClick={() => setToggle(!isToggle)}
+            onClick={() => HandleToggle()}
           >
             <AiOutlinePlusSquare size={20} />
+          </div>
+          <div className={LNCSS._wrap_m_btn}>
+            <div className={LNCSS._m_btn}>
+              <AiOutlineProject size={20} />
+            </div>
+            <div className={LNCSS._m_btn}>
+              <BsListTask size={20} />
+            </div>
+            <div className={LNCSS._m_btn}>
+              <MdTimer size={20} />
+            </div>
+            <div className={LNCSS._m_btn}>
+              <HiOutlineUsers size={20} />
+            </div>
+            <div className={LNCSS._m_btn}>
+              <AiOutlineSchedule size={20} />
+            </div>
+            <div className={LNCSS._m_btn}>
+              <IoIosAnalytics size={20} />
+            </div>
           </div>
         </div>
       )}
