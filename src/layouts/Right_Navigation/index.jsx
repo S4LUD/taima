b238543e61fc7282f61e2a902d04a21c3c86bea7
S4LUD <salud.lanceivan@gmail.com>
2022-10-21 17:39:47 +0800

    Create Vite@Latest React

--- src/layouts/Right_Navigation/index.jsx
+++ src/layouts/Right_Navigation/index.jsx
@@ -1,34 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import RNCSS from "./index.module.scss";
-import { AiOutlineFire } from "react-icons/ai";
+import { AiOutlineFire, BsCalendar2Event } from "react-icons/all";
 
 export default function Right_Navigation() {
+  const [isComment, setComment] = useState([{ test: "" }]);
+  const [isMeeting, setMeeting] = useState([]);
+
   return (
     <div className={RNCSS._rn_container}>
-      <div className={RNCSS._t_p_c_container}>
-        <div className={RNCSS._wrap_tpc}>
-          <div className={RNCSS._tp_count}>1K</div>
-          <div className={RNCSS._tpc_title}>Task</div>
-        </div>
-        <div className={RNCSS._wrap_tpc}>
-          <div className={RNCSS._tp_count}>14</div>
-          <div className={RNCSS._tpc_title}>Project</div>
-        </div>
-        <div className={RNCSS._wrap_tpc}>
-          <div className={RNCSS._tp_count}>168</div>
-          <div className={RNCSS._tpc_title}>Completed Task</div>
-        </div>
-      </div>
       <div className={RNCSS._comments_container}>
         <div className={RNCSS._comments_container_title}>
           <AiOutlineFire size={16} /> New comments
         </div>
-        <div className={RNCSS._wrap_comment}>
-          <div className={RNCSS._comment_title}>
-            <div className={RNCSS._title_tag}>Unread</div>
-            <span className={RNCSS._title_desc}>
-              Discord Bot Commands Discord Bot Commands Discord Bot Commands
-            </span>
+        {isComment.length !== 0 ? (
+          <div className={RNCSS._wrap_comment}>
+            <div className={RNCSS._comment_title}>
+              <div className={RNCSS._title_tag}>Unread</div>
+              <div className={RNCSS._title_desc}>
+                Discord Bot Commands Discord Bot Commands Discord Bot Commands
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className={RNCSS._empty_coms}>Empty comments</div>
+        )}
+      </div>
+      <div className={RNCSS._schdules_container}>
+        <div className={RNCSS._sched_title}>
+          <BsCalendar2Event size={14} />
+          Meeting Schedule
+        </div>
+        <div className={RNCSS._sched_container}>
+          <div className={RNCSS._wrap_sched_events}>
+            <div className={RNCSS._event_title_date}>
+              <div className={RNCSS._event_title_day}>21</div>
+              <div className={RNCSS._event_title_month}>Oct</div>
+            </div>
+            <div className={RNCSS._wrap_event_title}>
+              <div className={RNCSS._event_title}>
+                Discussion of Discord Bot Commands Discussion of Discord Bot
+                CommandsDiscussion of Discord Bot Commands Discussion of Discord
+                Bot CommandsDiscussion of Discord Bot Commands Discussion of
+                Discord Bot CommandsDiscussion of Discord Bot Commands
+                Discussion of Discord Bot CommandsDiscussion of Discord Bot
+                Commands Discussion of Discord Bot CommandsDiscussion of Discord
+                Bot Commands Discussion of Discord Bot CommandsDiscussion of
+                Discord Bot Commands Discussion of Discord Bot
+                CommandsDiscussion of Discord Bot Commands Discussion of Discord
+                Bot Commands
+              </div>
+              <div className={RNCSS._full_date}>Monday, October 24th</div>
+            </div>
+          </div>
+          <div className={RNCSS._wrap_sched_events}>
+            <div className={RNCSS._event_title_date}>
+              <div className={RNCSS._event_title_day}>21</div>
+              <div className={RNCSS._event_title_month}>Oct</div>
+            </div>
+            <div className={RNCSS._wrap_event_title}>
+              <div className={RNCSS._event_title}>
+                Discussion of Discord Bot Commands Discussion of Discord Bot
+                CommandsDiscussion of Discord Bot Commands Discussion of Discord
+                Bot CommandsDiscussion of Discord Bot Commands Discussion of
+                Discord Bot CommandsDiscussion of Discord Bot Commands
+                Discussion of Discord Bot CommandsDiscussion of Discord Bot
+                Commands Discussion of Discord Bot CommandsDiscussion of Discord
+                Bot Commands Discussion of Discord Bot CommandsDiscussion of
+                Discord Bot Commands Discussion of Discord Bot
+                CommandsDiscussion of Discord Bot Commands Discussion of Discord
+                Bot Commands
+              </div>
+              <div className={RNCSS._full_date}>Monday, October 24th</div>
+            </div>
+          </div>
+          <div className={RNCSS._wrap_sched_events}>
+            <div className={RNCSS._event_title_date}>
+              <div className={RNCSS._event_title_day}>21</div>
+              <div className={RNCSS._event_title_month}>Oct</div>
+            </div>
+            <div className={RNCSS._wrap_event_title}>
+              <div className={RNCSS._event_title}>
+                Discussion of Discord Bot Commands Discussion of Discord Bot
+                CommandsDiscussion of Discord Bot Commands Discussion of Discord
+                Bot CommandsDiscussion of Discord Bot Commands Discussion of
+                Discord Bot CommandsDiscussion of Discord Bot Commands
+                Discussion of Discord Bot CommandsDiscussion of Discord Bot
+                Commands Discussion of Discord Bot CommandsDiscussion of Discord
+                Bot Commands Discussion of Discord Bot CommandsDiscussion of
+                Discord Bot Commands Discussion of Discord Bot
+                CommandsDiscussion of Discord Bot Commands Discussion of Discord
+                Bot Commands
+              </div>
+              <div className={RNCSS._full_date}>Monday, October 24th</div>
+            </div>
           </div>
         </div>
       </div>
